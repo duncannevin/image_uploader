51d247d92f69fa214221b61a1f7e13f353c89e9d
Duncan Nevin <duncan.nevin@gmail.com>
2022-06-02 18:25:04 -0700

    Clear uploads in docker container

--- fe/lib/src/uploader_service.dart
+++ fe/lib/src/uploader_service.dart
@@ -1,15 +1,43 @@
 import 'dart:html';
+import 'dart:convert';
+import 'dart:async';
+import 'package:http/http.dart';
 
 enum UploadStage { IDLE, UPLOADING, UPLOADED }
 
+class Upload {
+  String id;
+  String location;
+
+  static Upload jsonDecode(String jsonStr) {
+    return json.decode(jsonStr);
+  }
+
+  Upload(this.id, this.location);
+}
+
 class UploaderService {
+  static final _headers = {'Content-Type': 'multipart/form-data'};
+  static final _uploaderLocation = 'http://localhost:3000';
+
   String uploadedImg = '';
 
   UploadStage stage = UploadStage.IDLE;
 
-  void uploadImage(File file) {
-    stage = UploadStage.UPLOADING;
-    uploadedImg = file.relativePath;
-    stage = UploadStage.UPLOADED;
+  Future<void> uploadImage(File file) async {
+    try {
+      stage = UploadStage.UPLOADING;
+      var formData = FormData();
+      formData.appendBlob('img', file);
+
+      final response = await HttpRequest.request(_uploaderLocation,
+          method: 'POST', sendData: formData).then((value) => value.response);
+
+      print(Upload.jsonDecode(response.toString()));
+    } catch (e) {
+      print(e.toString());
+    } finally {
+      stage = UploadStage.UPLOADED;
+    }
   }
 }
--- fe/web/main.dart
+++ fe/web/main.dart
@@ -1,12 +1,14 @@
 import 'package:angular/angular.dart';
 import 'package:ngcomponents/angular_components.dart';
+import 'package:http/browser_client.dart';
+import 'package:http/http.dart';
 import 'package:fe/app_component.template.dart' as ng;
 
 import 'main.template.dart' as self;
 
 // Example of a [root injector](https://angulardart.xyz/guide/dependency-injection#root-injector-providers)
 // [popupModule] is used in [MaterialTooltipDirective]
-@GenerateInjector([popupModule,])
+@GenerateInjector([ClassProvider(Client, useClass: BrowserClient), popupModule,])
 final InjectorFactory rootInjector = self.rootInjector$Injector;
 
 void main() {
